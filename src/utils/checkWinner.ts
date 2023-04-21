f976f3f37f09fb9e2915251c6d022551111d68b8
Nick Gonzalez <nick.gonse@gmail.com>
2023-04-21 00:33:14 -0600

    remove comments

--- src/utils/checkWinner.ts
+++ src/utils/checkWinner.ts
@@ -61,11 +61,6 @@ export const checkWinner = (board: State[][], activePlayer: State, col: number, 
     }
   }
 
-  // 0,5 - 1,4 - 2,3 - 3,2
-  // 1,5 - 2,4 - 3,3 - 4,2
-  // 2,5 - 3,4 - 4,3 - 5,2
-  // 3,5 - 4,4 - 5,3 - 6,2
-
   // diagonal => bottom-left -> top-right
   for (let tempCol = 0; tempCol < 4; tempCol++) {
     for (let tempRow = 5; tempRow > 2; tempRow--) {
@@ -81,12 +76,6 @@ export const checkWinner = (board: State[][], activePlayer: State, col: number, 
   }
 
   // diagonal => bottom-right -> top-left
-
-  // 6,5 - 5,4 - 4,3 - 3,2
-  // 5,5 - 4,4 - 3,3 - 2,2
-  // 4,5 - 3,4 - 2,3 - 1,2
-  // 3,5 - 2,4 - 1,3 - 0,2
-
   for (let tempCol = 6; tempCol > 2; tempCol--) {
     for (let tempRow = 5; tempRow > 2; tempRow--) {
       if (
